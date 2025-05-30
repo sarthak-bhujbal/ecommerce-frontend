fca1f7635a7e74181034bb2e78a796411a9c0e63
sarthak bhujbal <sarthak.bhujbal@simplifyvms.com>
2025-05-30 17:53:10 +0530

    hero section and navnar ready

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
 console.log("E-Commerce Website Loaded");
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger-menu');
+  const navMenu = document.querySelector('.nav-menu');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+});
